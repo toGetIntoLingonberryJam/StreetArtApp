8c0c9407ecb0366fb7acd6f3d8445ff690c61dd2
xX_Nikolay_Xx <homecomp2018@gmail.com>
2023-10-16 13:52:23 +0500

    intro design fix (for large screens)

--- lib/pages/intro/intro_auth_page.dart
+++ lib/pages/intro/intro_auth_page.dart
@@ -26,44 +26,46 @@ class IntroAuthPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            const Expanded(
-              flex: 4,
-              child: Image(image: AssetImage('assets/logo.png')),
-            ),
-            const SizedBox(height: 40),
-            const Text(
-              'Свидетели Стрит-Арта',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            Text(
-              content,
-              style: const TextStyle(fontSize: 17),
-              textAlign: TextAlign.center,
-            ),
-            const Expanded(flex: 2, child: SizedBox()),
-            FilledButton(
-              style: FilledButton.styleFrom(
-                minimumSize: const Size(300, 45),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Expanded(
+                flex: 4,
+                child: Image(image: AssetImage('assets/logo.png')),
               ),
-              onPressed: () => _navigateToAuth(context),
-              child:
-                  const Text('Войти в профиль', style: TextStyle(fontSize: 17)),
-            ),
-            const SizedBox(height: 10),
-            FilledButton.tonal(
-              style: FilledButton.styleFrom(
-                minimumSize: const Size(300, 45),
+              const SizedBox(height: 40),
+              const Text(
+                'Свидетели Стрит-Арта',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-              onPressed: () => _navigateToHome(context),
-              child: const Text('Позже', style: TextStyle(fontSize: 17)),
-            ),
-            const Expanded(flex: 2, child: SizedBox()),
-          ],
+              const SizedBox(height: 30),
+              Text(
+                content,
+                style: const TextStyle(fontSize: 17),
+                textAlign: TextAlign.center,
+              ),
+              const Expanded(flex: 2, child: SizedBox()),
+              FilledButton(
+                style: FilledButton.styleFrom(
+                  minimumSize: const Size(300, 45),
+                ),
+                onPressed: () => _navigateToAuth(context),
+                child: const Text('Войти в профиль',
+                    style: TextStyle(fontSize: 17)),
+              ),
+              const SizedBox(height: 10),
+              FilledButton.tonal(
+                style: FilledButton.styleFrom(
+                  minimumSize: const Size(300, 45),
+                ),
+                onPressed: () => _navigateToHome(context),
+                child: const Text('Позже', style: TextStyle(fontSize: 17)),
+              ),
+              const Expanded(flex: 2, child: SizedBox()),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/intro/intro_page.dart
+++ lib/pages/intro/intro_page.dart
@@ -15,38 +15,40 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            const Expanded(
-              flex: 4,
-              child: Image(image: AssetImage('assets/logo.png')),
-            ),
-            const SizedBox(height: 40),
-            const Text(
-              'Свидетели Стрит-Арта',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            Text(
-              content,
-              style: const TextStyle(fontSize: 17),
-              textAlign: TextAlign.center,
-            ),
-            const Expanded(flex: 2, child: SizedBox()),
-            FilledButton.tonal(
-              style: FilledButton.styleFrom(
-                minimumSize: const Size(300, 45),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Expanded(
+                flex: 4,
+                child: Image(image: AssetImage('assets/logo.png')),
               ),
-              onPressed: onButtonClicked,
-              child: const Text(
-                'Далее',
-                style: TextStyle(fontSize: 17),
+              const SizedBox(height: 40),
+              const Text(
+                'Свидетели Стрит-Арта',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-            ),
-            const Expanded(flex: 2, child: SizedBox()),
-          ],
+              const SizedBox(height: 30),
+              Text(
+                content,
+                style: const TextStyle(fontSize: 17),
+                textAlign: TextAlign.center,
+              ),
+              const Expanded(flex: 2, child: SizedBox()),
+              FilledButton.tonal(
+                style: FilledButton.styleFrom(
+                  minimumSize: const Size(300, 45),
+                ),
+                onPressed: onButtonClicked,
+                child: const Text(
+                  'Далее',
+                  style: TextStyle(fontSize: 17),
+                ),
+              ),
+              const Expanded(flex: 2, child: SizedBox()),
+            ],
+          ),
         ),
       ),
     );
