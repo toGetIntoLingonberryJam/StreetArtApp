f6787d1562a40329e05bb30d41e01bdb53dee913
nikolay-potehin <potehinnikolay@yandex.ru>
2023-11-28 23:22:59 +0500

    added Inter font

--- lib/constants.dart
+++ lib/constants.dart
@@ -14,21 +14,45 @@ const kSmallContainerRadius = 10.0;
 
 // TextStyles
 abstract class TextStyles {
-  static const title1 =
-      TextStyle(fontSize: 32, height: 1.25, fontWeight: FontWeight.bold);
-  static const title2 =
-      TextStyle(fontSize: 24, height: 1.25, fontWeight: FontWeight.w400);
+  static const title1 = TextStyle(
+    fontSize: 32,
+    height: 1.25,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Inter',
+  );
+  static const title2 = TextStyle(
+    fontSize: 24,
+    height: 1.25,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
 
-  static const headline1 =
-      TextStyle(fontSize: 20, height: 1.3, fontWeight: FontWeight.w600);
-  static const headline2 =
-      TextStyle(fontSize: 16, height: 1.25, fontWeight: FontWeight.w600);
+  static const headline1 = TextStyle(
+    fontSize: 20,
+    height: 1.3,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+  );
+  static const headline2 = TextStyle(
+    fontSize: 16,
+    height: 1.25,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Inter',
+  );
 
-  static const text = TextStyle(fontSize: 16, height: 1.25);
-  static const button = TextStyle(fontSize: 16, height: 1.25);
-  static const input = TextStyle(fontSize: 16, height: 1.25);
+  static const text =
+      TextStyle(fontSize: 16, height: 1.25, fontFamily: 'Inter');
+  static const button =
+      TextStyle(fontSize: 16, height: 1.25, fontFamily: 'Inter');
+  static const input =
+      TextStyle(fontSize: 16, height: 1.25, fontFamily: 'Inter');
 
-  static const textAdditional =
-      TextStyle(fontSize: 14, height: 1.4, fontWeight: FontWeight.w500);
-  static const caption = TextStyle(fontSize: 12, height: 1);
+  static const textAdditional = TextStyle(
+    fontSize: 14,
+    height: 1.4,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Inter',
+  );
+  static const caption =
+      TextStyle(fontSize: 12, height: 1, fontFamily: 'Inter');
 }
