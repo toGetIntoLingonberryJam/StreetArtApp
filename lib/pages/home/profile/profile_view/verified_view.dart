c6c7f8f9115ce532ba56ca96c996c341f4709893
nikolay-potehin <potehinnikolay@yandex.ru>
2023-12-07 01:56:47 +0500

    minor refactoring

--- lib/pages/home/profile/profile_view/verified_view.dart
+++ lib/pages/home/profile/profile_view/verified_view.dart
@@ -2,28 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:street_art_witnesses/constants.dart';
 import 'package:street_art_witnesses/src/providers/user_provider.dart';
-import 'package:street_art_witnesses/src/utils/utils.dart';
 import 'package:street_art_witnesses/pages/home/profile/widgets/profile_tile.dart';
 import 'package:street_art_witnesses/widgets/containers/user_tiles.dart';
 
 class VerifiedView extends StatelessWidget {
   const VerifiedView({super.key});
-
-  void _logout(BuildContext context) async {
-    final isLogout = await Utils.showWarning(
-          context,
-          title: 'Выйти из аккаунта',
-          content: 'Вы уверены, что хотите выйти из аккаунта?',
-        ) ??
-        false;
-
-    if (context.mounted && isLogout) {
-      await Utils.showLoading(
-        context,
-        context.read<UserProvider>().logout(),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
