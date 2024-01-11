dd7a15dec50deeeedad56e44ec300b4c22c227b1
nikolay-potehin <potehinnikolay@yandex.ru>
2024-01-11 21:53:38 +0500

    adds error_handler & custom_errors

--- lib/src/utils/custom_errors.dart
+++ lib/src/utils/custom_errors.dart
@@ -2,11 +2,33 @@ import 'package:dio/dio.dart';
 
 abstract class CustomErrors {
   static CustomError parse(DioException e) {
-    throw Exception('Method parse() is not implemented');
+    if (e.response == null) {
+      return CustomError.undefined();
+    }
+    return CustomError(e.response!);
   }
 }
 
-abstract class CustomError {
+final class CustomError {
+  CustomError(Response response) {
+    final details = response.data['detail'] as Map<String, dynamic>;
+    code = details['code'];
+    message = details['message'];
+    data = details['data'];
+  }
+
+  CustomError.undefined() {
+    code = '';
+    message = 'Произошла неизвестная ошибка :(';
+    data = null;
+  }
+
+  late final String code;
+  late final String message;
+  late final Map<String, dynamic>? data;
+
   @override
-  String toString();
+  String toString() {
+    return '$code: $message - $data';
+  }
 }
