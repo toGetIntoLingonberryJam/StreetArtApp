ac8528c164a22ae3487ac4f7c91b4f80f3733986
nikolay-potehin <potehinnikolay@yandex.ru>
2024-02-03 15:15:55 +0500

    adds artwork_ticket model

---
+++ lib/src/models/ticket.dart
@@ -0,0 +1,11 @@
+import 'package:street_art_witnesses/src/models/artwork/artwork.dart';
+
+class ArtworkTicket {
+  ArtworkTicket._(this.id, this.artwork);
+
+  final int id;
+  final Artwork artwork;
+
+  factory ArtworkTicket.fromJson(Map<String, dynamic> json) =>
+      ArtworkTicket._(json['id'], Artwork.fromTicket(json));
+}
