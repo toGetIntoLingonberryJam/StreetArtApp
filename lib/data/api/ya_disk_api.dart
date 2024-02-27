f09c6ae21574f888993594daa7a975d6f782e270
nikolay-potehin <potehinnikolay@yandex.ru>
2024-02-27 19:45:05 +0500

    ya_disk_api update (adds interceptor)

--- lib/data/api/ya_disk_api.dart
+++ lib/data/api/ya_disk_api.dart
@@ -1,40 +1,20 @@
 import 'package:dio/dio.dart';
-import 'package:street_art_witnesses/core/utils/logger.dart';
 
 abstract class YaDiskApi {
   static final _dio = Dio(
-    BaseOptions(baseUrl: 'https://cloud-api.yandex.net'),
+    BaseOptions(
+      baseUrl: 'https://cloud-api.yandex.net',
+      connectTimeout: const Duration(seconds: 5),
+    ),
   );
+  // )..interceptors.add(LoggerDioInterceptor());
 
-  static Future<Response?> _makeApiRequest(
-    Future<Response> request, {
-    required String url,
-  }) async {
-    Logger.apiCall(url);
-    try {
-      final result = await request;
-      Logger.apiResult(url, result.toString());
-      return result;
-    } on DioException catch (e) {
-      Logger.dioException(e, where: 'YaDiskApi');
-      return null;
-    }
-  }
-
-  static Future<Response?> get(
-    String url, {
-    Map<String, dynamic>? data,
-    Map<String, dynamic>? queryParameters,
+  static Future<Response?> Function(
+    String, {
+    CancelToken? cancelToken,
+    Object? data,
+    void Function(int, int)? onReceiveProgress,
     Options? options,
-  }) async {
-    return await _makeApiRequest(
-      _dio.get(
-        url,
-        data: data,
-        options: options,
-        queryParameters: queryParameters,
-      ),
-      url: url,
-    );
-  }
+    Map<String, dynamic>? queryParameters,
+  }) get get => _dio.get;
 }
