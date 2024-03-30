a293f7dd900abbc44805e4377f069fdcc0b50180
nikolay-potehin <potehinnikolay@yandex.ru>
2024-03-30 23:43:24 +0500

    removes loading animation from markers

--- lib/widgets/map/location_marker/controller.dart
+++ lib/widgets/map/location_marker/controller.dart
@@ -5,7 +5,6 @@ import 'package:street_art_witnesses/data/services/images_service.dart';
 import 'package:street_art_witnesses/data/services/settings_service.dart';
 import 'package:street_art_witnesses/modules/home/modules/map/controller.dart';
 import 'package:street_art_witnesses/widgets/containers/app_circle_avatar.dart';
-import 'package:street_art_witnesses/widgets/other/app_loading_indicator.dart';
 
 class LocationMarkerController extends GetxController {
   LocationMarkerController(this.location);
@@ -30,10 +29,7 @@ class LocationMarkerController extends GetxController {
         child: location.previewUrl == null
             ? const AppMarkerAvatar(child: Icon(Icons.color_lens_outlined))
             : image == null
-                ? AppMarkerAvatar(
-                    bgColor: Get.theme.colorScheme.surface,
-                    child: const AppLoadingIndicator(),
-                  )
+                ? AppMarkerAvatar(bgColor: Get.theme.colorScheme.secondary)
                 : AppMarkerAvatar(image: image),
       );
 }
