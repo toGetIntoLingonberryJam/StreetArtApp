9aa5db6e5693786200899ee1a7ff55496af8a5ec
nikolay-potehin <potehinnikolay@yandex.ru>
2024-05-01 21:29:37 +0500

    updates collection on auth state changes

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -7,6 +7,7 @@ import 'package:street_art_witnesses/data/api/local_store_datasource.dart';
 import 'package:street_art_witnesses/data/models/user.dart';
 import 'package:street_art_witnesses/data/services/local_store_service.dart';
 import 'package:street_art_witnesses/modules/auth/check_email/controller.dart';
+import 'package:street_art_witnesses/modules/home/modules/collection/controller.dart';
 
 class AuthService extends GetxService {
   late final Rx<User> _user;
@@ -16,6 +17,11 @@ class AuthService extends GetxService {
     final token = await LocalStoreService.retrieveToken();
     final initUser = token == null ? User.guest() : await _authenticate(token: token);
     _user = initUser.obs;
+
+    // On user auth state updates
+    user.listen((updatedUser) {
+      Get.find<CollectionController>().loadAll();
+    });
   }
 
   Future<void> updateUser() async {
