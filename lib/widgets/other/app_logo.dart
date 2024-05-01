a1ffb81158bdbd6adde598f5c5af128c8e5e1524
nikolay-potehin <potehinnikolay@yandex.ru>
2024-05-01 21:20:40 +0500

    logo size fix

--- lib/widgets/other/app_logo.dart
+++ lib/widgets/other/app_logo.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class AppLogo extends StatelessWidget {
   const AppLogo({super.key});
 
+  final double width = 128;
+  final double height = 128;
+
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/logo_light.png');
+    return Image.asset('assets/logo_light.png', width: width, height: height);
   }
 }
